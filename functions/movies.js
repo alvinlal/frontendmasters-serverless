16bb890278eeebccf6d687c95621502029115607
alvinlal <alvinzzz2001@gmail.com>
2020-09-10 13:30:18 +0530

    finished 2.3:added bloop movies

--- functions/movies.js
+++ functions/movies.js
@@ -3,6 +3,6 @@ const movies = require("../data/movies.json");
 exports.handler = async () => {
   return {
     statusCode: 200,
-    movies: JSON.stringify(movies),
+    body: JSON.stringify(movies),
   };
 };
